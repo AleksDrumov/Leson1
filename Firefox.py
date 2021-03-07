388329a2a596d2e2544f019e7e6c52baee015ba0
AleksDrumov <79414090+AleksDrumov@users.noreply.github.com>
2021-03-07 21:50:51 +0200

    Update Firefox.py

--- Firefox.py
+++ Firefox.py
@@ -7,3 +7,18 @@ while suma < 1:
     count = count + 1
     print(suma)
 print(count)
+
+#Вторая часть
+suma = 0 
+firstNumber = 100
+count = 0
+RAM = int(input('Сока у тя опративки в мегабайтах'))
+while suma < RAM:
+    suma = suma + firstNumber
+    count = count + 1
+if count > 199:
+    print('Чего-чего?!')
+elif RAM < 0:
+    print('Ti durachok')
+else:
+    print('Ну ок...')
