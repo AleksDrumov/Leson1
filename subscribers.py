a572f7a2ebdd82222ca32490c60178e121385d83
AleksDrumov <79414090+AleksDrumov@users.noreply.github.com>
2021-03-07 21:41:11 +0200

    Update subscribers.py

--- subscribers.py
+++ subscribers.py
@@ -1,19 +1,9 @@
-import time
-import os
-import random
-rang = range(0,3)
-Minecrafter1999_subscribers = 2000
-DoctorHto_subscribers = 50
-while True:
-    Minecrafter1999_subscribers = Minecrafter1999_subscribers + random.choice(rang)
-    DoctorHto_subscribers = DoctorHto_subscribers + random.choice(rang)
-    dif_Minecrafter1999_subscribers_and_DoctorHto_subscribers = Minecrafter1999_subscribers - DoctorHto_subscribers
-    print("у Minecrafter1999", Minecrafter1999_subscribers,"подписчиков")
-    print("у DoctorHto", DoctorHto_subscribers,"подписчиков")
-    print("У Minecrafter1999 на", dif_Minecrafter1999_subscribers_and_DoctorHto_subscribers,"чем у DoctorHto")
-    time.sleep(1)
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
+Minecrafter1999_Sucribers = 2000
+DoctorHto_Sucribers = 50
+riznitza = DoctorHto_Sucribers - Minecrafter1999_Sucribers
+if riznitza < 0:
+    riznitza = riznitza * -1
+print(riznitza)
     
     
     
